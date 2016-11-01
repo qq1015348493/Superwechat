41f90f8c99b2e710d1b94dc90681041eac2bf7cf
zhang <tx1015348493@163.com>
2016-11-01 22:37:09 +0800

    添加butterkniff 和工具类

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/utils/SuperWeChatApplication.java
@@ -0,0 +1,12 @@
+package cn.ucai.superwechat.utils;
+
+import android.content.Context;
+
+/**
+ * Created by User on 2016/11/1.
+ */
+public class SuperWeChatApplication {
+    public static Context getInstance() {
+        return null;
+    }
+}
